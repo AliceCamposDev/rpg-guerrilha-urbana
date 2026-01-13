b619a088765d3c2fdcbb1e5b380b97ebbdc3de46
Alice Campos <allicefcampos@gmail.com>
2026-01-12 23:04:34 -0300

    need a home :/

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,10 @@
 import { Routes } from '@angular/router';
 import { CharacterSheetModule } from './features/character-sheet/character-sheet-module';
 import { CharacterSheetView } from './features/character-sheet/components/character-sheet-view/character-sheet-view';
+import { HomeView } from './features/home/components/home-view/home-view';
 
 export const routes: Routes = [
-  { path: 'character-sheet', loadChildren: () => CharacterSheetModule }
+  { path: 'character-sheet', loadChildren: () => CharacterSheetModule },
+  { path: '', component: HomeView },
+
 ];
